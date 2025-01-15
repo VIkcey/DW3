4599fc22899b23b9073eb1d2f71f83bd14a46633
VIkcey <163313547+VIkcey@users.noreply.github.com>
2025-01-15 10:58:22 -0500

    add some new variables

--- hello-world.php
+++ hello-world.php
@@ -12,7 +12,25 @@ $welcome_message = "Bienvenue a DW3, nombre des messages dans electronique boite
 $int_message = 7;
 $true_bool = true;
 $false_bool = false;
-?>
+
+
+$fruit_list
+    = [
+        'Banana', // index: 0
+        'Apple',  // index: 1
+        'Orange', // index: 2
+        'Mango', // index: 3
+        'Grapes', // index: 4
+        'Pineapple' // index: 5
+    ];
+
+$student = [
+    "name" => "John Smith",
+    "age" => 21,
+    "Program" => "Computer Science",
+]
+
+    ?>
 
 <body>
     <H1>Hello World</H1>
@@ -20,6 +38,8 @@ $false_bool = false;
     <p><?= $int_message ?></p>
     <p>Boolean true: <?= $true_bool ?></p>
     <p>Boolean false: <?= $false_bool ?></p>
+    <p>Mon favori fruit est: <?= $fruit_list[3] ?></p>
+    <p>Étudiant nom: <?= $student['name'] ?></p>
 </body>
 
 </html>
