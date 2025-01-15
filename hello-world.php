527196982cf766b38630f5633e010646fcf59152
VIkcey <163313547+VIkcey@users.noreply.github.com>
2025-01-15 10:18:25 -0500

    add bool value

--- hello-world.php
+++ hello-world.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hello World</title>
-        <link rel="stylesheet" type="text/css" href="main.css">
-    </head>
-    <?php
-       $welcome_message = "Welcome to DW3!";
-       echo $welcome_message;
-    ?>
-    <body>
-        <H1>Hello World</H1>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<?php
+$welcome_message = "Bienvenue a DW3, nombre des messages dans electronique boite";
+$int_message = 7;
+$true_bool = true;
+$false_bool = false;
+?>
+
+<body>
+    <H1>Hello World</H1>
+    <p><?= $welcome_message ?></p>
+    <p><?= $int_message ?></p>
+    <p>Boolean true: <?= $true_bool ?></p>
+    <p>Boolean false: <?= $false_bool ?></p>
+</body>
+
 </html>
