49e65483b77c577870e9ae8814caa19aa01740ae
VIkcey <163313547+VIkcey@users.noreply.github.com>
2025-01-20 20:22:14 -0500

    add new files

---
+++ comparison-operators.php
@@ -0,0 +1,28 @@
+<?php
+// Equality ( == or ===)
+$a = 5;
+$b = "5";
+
+
+
+
+// Not equal (!= or !==)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>$a == $b: <?= $a == $b ?></p>
+    <p>$a === $b: <?= $a === $b ?></p>
+    <p>$a != $b: <?= $a != $b ?></p>
+    <p>$a !== $b: <?= $a !== $b ?></p>
+</body>
+
+</html>
